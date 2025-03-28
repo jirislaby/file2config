3a76aeac84f893a357532edbb25de01005c19a2e
Jiri Slaby <jirislaby@gmail.com>
2025-03-28 11:10:02 +0100

    getopt

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
+#include <getopt.h>
+
 #include "treewalker/treewalker.h"
 
 using namespace std;
 
 unsigned verbose;
 
+static void usage(const char *prgname)
+{
+	std::cout << prgname << ": [-v] [start_path]\n";
+}
+
 int main(int argc, char **argv)
 {
+	const struct option opts[] = {
+		{ "verbose", 0, nullptr, 'v' },
+		{}
+	};
+	int opt;
+	while ((opt = getopt_long(argc, argv, "v", opts, nullptr)) >= 0) {
+		switch (opt) {
+		case 'v':
+			verbose++;
+			break;
+		default:
+			usage(argv[0]);
+			return 0;
+		}
+
+	}
+
 	std::filesystem::path path{"."};
-	if (argc > 1)
-		path = argv[1];
+	if (argc > optind)
+		path = argv[optind];
 
 	TW::TreeWalker tw(path);
 	tw.walk();

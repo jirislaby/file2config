1f30f77dd4429a72c8cb0bca3ed3681aff172ef1
Jiri Slaby <jirislaby@gmail.com>
2025-07-11 12:26:04 +0200

    main: use PatchesAuthors from helpers
    The code has just moved there.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include <sl/kerncvs/Branches.h>
+#include <sl/kerncvs/PatchesAuthors.h>
 #include <sl/kerncvs/SupportedConf.h>
 #include <sl/git/Git.h>
 #include <sl/helpers/PushD.h>
@@ -253,155 +254,19 @@ static std::optional<SlKernCVS::SupportedConf> getSupported(const SlGit::Repo &r
 	return SlKernCVS::SupportedConf { *suppConf };
 }
 
-class PatchesAuthors {
-public:
-	using Map = std::map<std::string, std::map<std::string, unsigned int>>;
-
-	PatchesAuthors(bool dumpRefs, bool reportUnhandled) : dumpRefs(dumpRefs),
-		reportUnhandled(reportUnhandled)
-	{}
-
-	int processPatch(const std::filesystem::path &file, const std::string &content);
-
-	const Map &HoH() const { return m_HoH; }
-	const Map &HoHReal() const { return m_HoHReal; }
-	const Map &HoHRefs() const { return m_HoHRefs; }
-
-	int processAuthors(const std::unique_ptr<SQL::F2CSQLConn> &sql, const std::string &branch,
-			   const SlGit::Repo &repo, const SlGit::Commit &commit);
-private:
-	const bool dumpRefs;
-	const bool reportUnhandled;
-	static const std::regex REInteresting;
-	static const std::regex REFalse;
-	static const std::regex REGitFixes;
-	static const std::regex REInvalRef;
-	Map m_HoH;
-	Map m_HoHReal;
-	Map m_HoHRefs;
-};
-
-const std::regex PatchesAuthors::REInteresting("^\\s*(?:From|Cc|Co-developed-by|Acked|Acked-by|Modified-by|Reviewed-by|Reviewed-and-tested-by|Signed-off-by):.*[\\s<]([a-z0-9_.-]+\\@suse\\.[a-z]+)",
-	      std::regex_constants::icase);
-const std::regex PatchesAuthors::REFalse("(?:lore|lkml)\\.kernel|patchwork\\.ozlabs|^\\[|^(?:Debugged-by|Evaluated-by|Improvements-by|Link|Message-ID|Patch-mainline|Reported-and-tested-by|Reported-by|Return-path|Suggested-by|Tested-by):|thanks|:$",
-	      std::regex_constants::icase);
-const std::regex PatchesAuthors::REGitFixes("^References:.*(?:(?:git|stable)[- ]fixes|stable-\\d|b[ns]c[#](?:1012628|1051510|1151927|1152489))",
-	   std::regex_constants::icase);
-const std::regex PatchesAuthors::REInvalRef("FATE#|CVE-|jsc#|XSA-", std::regex_constants::icase);
-
-int PatchesAuthors::processPatch(const std::filesystem::path &file, const std::string &content)
+static int processAuthors(const std::unique_ptr<SQL::F2CSQLConn> &sql, const std::string &branch,
+			  const SlGit::Repo &repo, const SlGit::Commit &commit, bool dumpRefs,
+			  bool reportUnhandled)
 {
-	if (F2C::verbose > 2)
-		std::cout << __func__ << ": " << file << '\n';
-	std::set<std::string> patchEmails;
-	std::set<std::string> patchRefs;
-	bool gitFixes = false;
-	std::istringstream iss(content);
-	std::string line;
-	std::smatch m;
+	SlKernCVS::PatchesAuthors PA{repo, dumpRefs, reportUnhandled};
 
-	while (std::getline(iss, line)) {
-		if (std::regex_search(line, m, REInteresting)) {
-			patchEmails.insert(m[1]);
-			continue;
-		}
-		if (SlHelpers::String::startsWith(line, "---"))
-			break;
-		if (std::regex_search(line, REGitFixes)) {
-			gitFixes = true;
-		} else if (dumpRefs) {
-			static const std::string references { "References:" };
-			if (SlHelpers::String::startsWith(line, references))
-				for (const auto &ref: SlHelpers::String::split(line.substr(references.size()),
-									" \t,;"))
-					patchRefs.insert(ref);
-		}
-
-		if (reportUnhandled && line.find("@suse.") != std::string::npos &&
-				!std::regex_search(line, REFalse))
-			std::cerr << file << ": unhandled e-mail in '" << line << "'\n";
-	}
-
-	for (const auto &ref : patchRefs)
-		for (const auto &email : patchEmails)
-			if (!std::regex_search(ref, REInvalRef))
-				m_HoHRefs[email][ref]++;
-
-	while (std::getline(iss, line)) {
-		static const std::string prefix { "+++ b/" };
-		if (!SlHelpers::String::startsWith(line, prefix))
-			continue;
-		if (!SlHelpers::String::endsWith(line, ".c") &&
-				!SlHelpers::String::endsWith(line, ".h"))
-			continue;
-
-		auto cfile = line.substr(prefix.length());
-		if (SlHelpers::String::startsWith(cfile, "/dev"))
-			std::cerr << __func__ << ": " << file << ": " << cfile << '\n';
-		for (const auto &email : patchEmails) {
-			m_HoH[email][cfile]++;
-			if (gitFixes)
-				m_HoHReal[email][cfile]; // add so it exists
-			else
-				m_HoHReal[email][cfile]++;
-		}
-	}
-
-	return 0;
-}
-
-int PatchesAuthors::processAuthors(const std::unique_ptr<SQL::F2CSQLConn> &sql,
-				   const std::string &branch, const SlGit::Repo &repo,
-				   const SlGit::Commit &commit)
-{
-	PatchesAuthors PA(dumpRefs, reportUnhandled);
-
-	SlGit::Tree tree;
-	tree.ofCommit(commit);
-
-	SlGit::TreeEntry patchesSuseTreeEntry;
-	if (patchesSuseTreeEntry.byPath(tree, "patches.suse/"))
-		return -1;
-	if (patchesSuseTreeEntry.type() != GIT_OBJECT_TREE)
-		return -1;
-
-	SlGit::Tree patchesSuseTree;
-	if (patchesSuseTree.lookup(repo, patchesSuseTreeEntry))
-		return -1;
-	auto ret = patchesSuseTree.walk([&repo, &PA](const std::string &root,
-					const SlGit::TreeEntry &entry) -> int {
-		SlGit::Blob blob;
-		if (blob.lookup(repo, entry))
-			return -1000;
-
-		return PA.processPatch(root + entry.name(), blob.content());
+	return PA.processAuthors(commit, [&sql](const std::string &email) -> int {
+		return sql->insertUser(email);
+	}, [&branch, &sql](const std::string &email, const std::filesystem::path &path,
+			unsigned count, unsigned realCount) -> int {
+		return sql->insertUFMap(branch, email, path.parent_path().string(),
+				     path.filename().string(), count, realCount);
 	});
-	if (ret)
-		return -1;
-
-	for (const auto &pair : PA.HoHRefs())
-		for (const auto &refPair : pair.second)
-			if (refPair.second > 100) {
-				std::cout << std::setw(30) << pair.first <<
-					     std::setw(40) << refPair.first <<
-					     std::setw(5) << refPair.second << '\n';
-			}
-
-	for (const auto &pair : PA.HoH()) {
-		const auto &email = pair.first;
-		const auto &realMap = PA.HoHReal().at(email);
-		if (sql->insertUser(email))
-			return -1;
-
-		for (const auto &pairSrc : pair.second) {
-			std::filesystem::path path(pairSrc.first);
-
-			if (sql->insertUFMap(branch, email, path.parent_path().string(),
-					     path.filename().string(), pairSrc.second,
-					     realMap.at(pairSrc.first)))
-				return -1;
-		}
-	}
 
 	return 0;
 }
@@ -432,10 +297,9 @@ int processBranch(const std::string &branchNote, const std::unique_ptr<SQL::F2CS
 		TW::TreeWalker tw(root, *visitor);
 		tw.walk();
 
-		std::cout << "== " << branchNote << " -- Detecting authors of patches ==\n";//, 'green');
 		if (sql) {
-			PatchesAuthors PA{dumpRefs, reportUnhandled};
-			PA.processAuthors(sql, branch, repo, commit);
+			std::cout << "== " << branchNote << " -- Detecting authors of patches ==\n";//, 'green');
+			processAuthors(sql, branch, repo, commit, dumpRefs, reportUnhandled);
 		}
 	}
 
